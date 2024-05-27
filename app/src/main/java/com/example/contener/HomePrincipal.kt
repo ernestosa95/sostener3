6e0948801fa6c3d2385acad8cb56b847daec4bfc
ernesto <ridelernesto@gmail.com>
2024-05-26 21:19:40 -0300

    TODO: - Inicio de sesion con user y contraseña - Embeber un video -> LISTO - Reproducir un video propio de la app - Crear pantallas de diseño, agrego foto de perfil - Prellenado de formulario

--- app/src/main/java/com/example/contener/HomePrincipal.kt
+++ app/src/main/java/com/example/contener/HomePrincipal.kt
@@ -1,17 +1,27 @@
 package com.example.contener
 
 import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
 import android.preference.PreferenceManager
+import android.util.Log
 import android.view.WindowManager
+import android.webkit.WebView
 import android.widget.Button
-import android.widget.MediaController
+import android.widget.ImageView
 import android.widget.Toast
-import android.widget.VideoView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import java.net.URL
+
 
 class HomePrincipal : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_principal)
@@ -22,6 +32,20 @@ class HomePrincipal : AppCompatActivity() {
         val myPreferences = PreferenceManager.getDefaultSharedPreferences(this@HomePrincipal)
         Toast.makeText(this, myPreferences.getString("p1", "unknown"), Toast.LENGTH_SHORT).show()
 
+        val perfil = findViewById<ImageView>(R.id.perfilimage)
+        val auth = Firebase.auth
+        val user = auth.currentUser
+        if (user != null) {
+            val policy = ThreadPolicy.Builder().permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+            val txt = user.photoUrl
+            val newurl = URL(txt.toString())
+            val mIcon_val = BitmapFactory.decodeStream(newurl.openConnection().getInputStream())
+            perfil.setImageBitmap(mIcon_val)
+
+
+        }
+
         val formButton = findViewById<Button>(R.id.formButton)
         formButton.setOnClickListener {
             val intent = Intent(this, formularioHome::class.java)
@@ -29,12 +53,12 @@ class HomePrincipal : AppCompatActivity() {
             finish()
         }
 
-        /*val mVideoView : VideoView = findViewById(R.id.videoView);
-        //de forma alternativa si queremos un streaming usar
-        mVideoView.setVideoURI(Uri.parse("https://www.youtube.com/watch?v=KBo1pyugG88"));
-        //mVideoView.setVideoPath("/mnt/sdcard/video.mp4");
+        val webView : WebView = findViewById(R.id.web);
+        val video = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/V2KCAfHjySQ?si=qkP3WXXxZEh9EDk-\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>"
+        webView.loadData(video, "text/html", "utf-8")
+        webView.settings.javaScriptEnabled = true
 
-        val mediaController = MediaController(this)
-        mVideoView.setMediaController(mediaController);*/
     }
+
+
 }
